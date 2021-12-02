7ed3f0a38e984b12237a49f3451530f610da480e
Marc-Andre Lafortune <github@marc-andre.ca>
2021-12-01 21:04:12 -0500

    Format

--- erts/emulator/beam/erl_map.h
+++ erts/emulator/beam/erl_map.h
@@ -1,8 +1,8 @@
 /*
  * %CopyrightBegin%
- * 
+ *
  * Copyright Ericsson AB 2014-2017. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * %CopyrightEnd%
  */
 
@@ -43,7 +43,7 @@ typedef struct flatmap_s {
 /* map node
  *
  * -----------
- * Eterm   THING	
+ * Eterm   THING
  * Uint    size
  * Eterm   Keys -> {K1, K2, K3, ..., Kn} where n = size
  * ----
